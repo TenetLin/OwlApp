40bd7469c17a7f65ef8b8afcdde150c6d7a95729
jeffeliu <liuzhifei@upchina.com>
2019-02-20 16:46:04 +0800

    首页日期选择

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,27 +6,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    day_datas: [],
     tabbar: {},
     navigationBarTitle: '主页',
     navigationBarHeight: (app.statusBarHeight + 44) + 'px',
     weathers: true,
     swiper_height: '600px',
     homes:false,
-    current_date: '',
     select_date: '',
+    calendar_select: '',
+    hide_calendar: true,
+    data: []
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
     app.editTabbar()
-
+    let select_date, data
     try {
-      const data = JSON.parse(wx.getStorageSync('list') || '{}')
-      this.setData(data)
+      const all_data = JSON.parse(wx.getStorageSync('list') || '{}')
+      if (all_data['default_date']) {
+        select_date = moment(all_data['default_date'], 'YYYYMMDD')
+        data = all_data[select_date] || []
+      }
     } catch (ex) { console.log('get data from cache error, key=list, ex=', ex) }
-    
+
+    if (!select_date || !select_date.isValid()) {
+      select_date = moment()
+    }
+    data = data || []
+    this.setData({ select_date: select_date.format('YYYY年MM月DD日'), calendar_select: select_date.format('YYYY-MM-DD'), data })
+    this.getOneDay(select_date.format('YYYYMMDD'))
   },
 
   /**
@@ -41,30 +51,12 @@ Page({
       name: 'getlist',
       data: { date },
       success({ errMsg, result }) {
-        
         console.log('get list suc', errMsg, result)
-        
-        let day_datas = that.data.day_datas
-
-        day_datas = day_datas.map(item => {
-
-          if (item.date === date) {
-            item.data = result
-          }
-          return item
-        })
-
-        that.setData({ day_datas })
-
-        that.save()
-        console.log('data=', that.data)
+        that.setData({ data: result })
+        that.save(date, result, true)
+        console.log('data=', date, result)
       },
-
-      fail(error) {
-
-        console.log('get list error', error)
-
-      }
+      fail(error) { console.log('get list error', error)}
     })
   },
 
@@ -115,10 +107,37 @@ Page({
   onShareAppMessage: function () {
 
   },
+
+  /**
+   * 展示日历菜单
+   */
+  show_calendar: function (e) {
+    const hide_calendar = this.data.hide_calendar
+    this.setData({ hide_calendar: !hide_calendar })
+  },
+
+  /**
+   * 选择日期
+   */
+  select_date: function (e) {
+    const { date } = e.detail
+    const _date = moment(date, 'YYYY-MM-DD').format('YYYYMMDD')
+    this.setData({ hide_calendar: true, select_date: moment(date, 'YYYY-MM-DD').format('YYYY年MM月DD日')})
+    this.getOneDay(_date)
+  },
   
-  save: function () {
+  /**
+   * 存储数据
+   * date: 日期
+   * data: 数据内容
+   * isDefault: 是否是默认日期
+   */
+  save: function (date, data, isDefault) {
     try {
-      wx.setStorageSync('list', JSON.stringify(this.data))
+      const all_data = JSON.parse(wx.getStorageSync('list') || '{}')
+      all_data[date] = data
+      if (isDefault) all_data['default_date'] = date
+      wx.setStorageSync('list', JSON.stringify(all_data))
     } catch (ex) {
       console.log('get data to cache error, key=list, ex=', ex)
     }
